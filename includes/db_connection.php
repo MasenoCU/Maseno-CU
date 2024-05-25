a35590a3365cd51caad0dc144e7df593fc636bba
Fidel-Kisevu <fideliskisevu@gmail.com>
2024-05-24 22:03:01 -0700

    did some marging and added includes files

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,14 +1,13 @@
 <?php
-// to conne3ct to the database replace the value in quotes with the atual values.
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mucu_db";
+$dbname = "MUCUWEB";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection for any  errors arising.
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
